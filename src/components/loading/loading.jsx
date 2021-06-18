f4a2f47bf91347db13191efe77a1b2446e64b431
Johnnatan Reyes Garcia <tanamix22@gmail.com>
2021-06-18 17:02:35 -0500

    create proyect component

---
+++ src/components/loading/loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Loader} from 'react-bulma-components'
+
+const loading = () => {
+    return (
+        <Loader> Cargando... </Loader>
+    )
+}
+
+export default loading
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,16 @@
 import Head from 'next/head';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import Layout from '../components/layout/layout';
 import { store } from '../context/store';
+import Button from '../components/button/button';
+import Loading from '../components/loading/loading';
+
+
 
 export default function Home() {
   const { state } = useContext(store);
   const { theme } = state;
+  const [isLoading,setLoading] = useState(false)
 
   return (
     <>
@@ -15,20 +20,30 @@ export default function Home() {
       </Head>
 
       <Layout>
-        <div>
-          <h1>Projects</h1>
-          <h1>Projects</h1>
-          <h1>Projects</h1>
-          <h1>Projects</h1>
-          <h1>Projects</h1>
-          <h1>Projects</h1>
-          <h1>Projects</h1>
-          <h1>Projects</h1>
-          {/* <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1> */}
+        <div className='container'>
+          <div className='container__button'>
+            <h1>Projects</h1> <Button/>
+          </div>
+
+          
+
+          
+          {
+            isLoading
+            ? <Loading />
+            : <>
+            <div className='container__content'>
+            <h1>Project1</h1>
+            <h1>Detail</h1>
+          </div>  
+
+          <div className='container__content'>
+            <h1>Project2</h1>
+            <h1>Detail</h1>
+          </div>
+            </>
+          }
+
         </div>
       </Layout>
     </>
