07d479aaf3290964b9cba7e3298931fb4dfc0695
Antonio DroCode <antuansabe@gmail.com>
2021-06-21 19:22:51 -0500

    fixing bugs pt2

--- src/pages/newProject.jsx
+++ src/pages/newProject.jsx
@@ -14,11 +14,11 @@ export default function Login() {
             <h1 className='container__h1'>New Project</h1>
             <h4>New set up a calendar</h4> 
                 <p>What kind of project is it?</p>
-                    <input className="login__container--input" placeholder="Select a type of project"></input> 
+                    <input className="login__container--input" placeholder="  Select a type of project"></input> 
                 <p>When will you complete it?</p> 
-                    <input className="login__container--input" placeholder="Enter a deadline"></input>
+                    <input className="login__container--input" placeholder="  Enter a deadline"></input>
                 <p>Select calendar to work with</p> 
-                    <input className="login__container--input" placeholder="Please, select a calendar"></input>
+                    <input className="login__container--input" placeholder="  Please, select a calendar"></input>
                     <button className= 'btn btn-primary'>Create Project</button>     
     </div>
 </Layout>  
