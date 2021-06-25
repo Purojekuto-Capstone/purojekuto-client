c19aa2d44c27d28e634c60ce2431eeedc84dcd06
Antonio DroCode <antuansabe@gmail.com>
2021-06-25 14:02:19 -0500

    refactor(Statistics): change Highcharts for Recharts to render a charts

--- src/pages/statistics.jsx
+++ src/pages/statistics.jsx
@@ -1,17 +1,8 @@
 import React from 'react';
 import Head from 'next/head';
 import Layout from '../components/layout/layout';
-import Highcharts from 'highcharts';
-import HighchartsReact from 'highcharts-react-official';
-
-const options = {
-  series: [
-    {
-      name: 'Profit',
-      data:[100,200,30,100,30,50,100]
-    }
-  ]
-};
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid } from 'recharts';
+const data = [{name: 'Proyectos', uv: 40, pv: 2400, amt: 2400}];
 
 export default function Statistics() {
   return (
@@ -22,11 +13,17 @@ export default function Statistics() {
       </Head>
 
       <Layout>
-      
-        <HighchartsReact highcharts={Highcharts} options={{options}} />
-      
+      <BarChart width={600} height={300} data={data}>
+        <XAxis dataKey="name" stroke="#F89F29" />
+        <YAxis />
+        <Tooltip wrapperStyle={{ width: 100, backgroundColor: '#ccc' }} />
+        <Legend width={100} wrapperStyle={{ top: 40, right: 20, backgroundColor: '#f5f5f5', border: '1px solid #d5d5d5', borderRadius: 3, lineHeight: '40px' }} />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <Bar dataKey="uv" fill="#F89F29" barSize={30} />
+  </BarChart>
       </Layout>
     </>
   );
 }
+
 
