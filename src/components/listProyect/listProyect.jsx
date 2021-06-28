dd7858372bf8142b267d82012e5a05374fbd89c4
Johnnatan Reyes Garcia <tanamix22@gmail.com>
2021-06-28 18:36:38 -0500

    create signOut icon and link

--- src/components/listProyect/listProyect.jsx
+++ src/components/listProyect/listProyect.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import ProyectsJson from '../../data/projects.json';
 
 const listProyect = ({ proyects }) => {
+  const router = useRouter();
+
+  const handleOnCardClick = (id) => {
+    console.log(id)
+    router.push(`/project/${id}`);
+  };
+
   console.log(proyects)
+
   return (
     <div className="container__list">
-      {proyects.map(({ id, project_name, end_date }) => (
-        <div className="container__content" id={id} key={id}>
+      {proyects.map(({ id, activity_name, end_date }) => (
+        <div className="container__content--project" 
+        id={id} key={id} onClick={() => handleOnCardClick(id)}>
           <div className="container__deahtline">
             <div></div>
           </div>
-          <div className="container__name">{project_name}</div>
+          <div className="container__name">{activity_name}</div>
           <div className="container__deahtline">{end_date}</div>
           <div className="container__more">..</div>
         </div>
