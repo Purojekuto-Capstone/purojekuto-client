2d031c2bfb999f60fe70bd0f00743dca98f3c5ed
Anthony Gonzalez <thomasnrggo@gmail.com>
2021-06-28 22:03:13 -0500

    hotfix

--- src/pages/login/[token].jsx
+++ src/pages/login/[token].jsx
@@ -10,23 +10,12 @@ export default function loginAuthRedirect(props) {
   const { token } = router.query
 
   useEffect(() => {
-    // var obj = {a: 'a', b: 'b'};
-    // let encoded = btoa(JSON.stringify(obj))
-    // ewogICJ0b2tlbiI6ICJqYW5zamFqc2FzIiwKICAic2x1YiI6ICJhbXNhc2FzIgp9
-    if(token) {
-      let authInfo = JSON.parse(atob(token))
-      console.log(authInfo);
-      console.log('token =>',authInfo.token);
-
-    }
-
     if(isAuth) {
       router.push('/')
     } else {
       if(token){
         setTimeout(() => {
           let user = JSON.parse(atob(token))
-
           dispatch({ type: 'SET_USER', payload: user })
           router.push('/')
         }, 800);
