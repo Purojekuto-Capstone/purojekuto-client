77882662b0fd799f2ee0d44b18d530603402960f
Anthony Gonzalez <thomasnrggo@gmail.com>
2021-06-29 23:46:20 -0500

    created event's routes, /project//event/new and /project//event/

---
+++ src/pages/project/[project]/event/new.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../../../components/layout/layout'
+
+export default function NewEvent(props) {
+  return (
+    <>
+      <Layout>
+        Aqui va el formulario de nuevo proyecto
+      </Layout>
+    </>
+  )
+}
