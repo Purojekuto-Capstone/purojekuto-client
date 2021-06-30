3fd7c57b042f107690cfa792168a887391fafe86
Anthony Gonzalez <thomasnrggo@gmail.com>
2021-06-29 23:55:00 -0500

    copy new project form into new event page

--- src/pages/project/[project]/event/new.jsx
+++ src/pages/project/[project]/event/new.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import Layout from '../../../../components/layout/layout'
+import Link from 'next/link'
 
 export default function NewEvent(props) {
   return (
     <>
       <Layout>
-        Aqui va el formulario de nuevo proyecto
+      <div className="container">
+        <Link href="/" >
+          <a>←Back</a>
+          </Link>
+          <h1 className="container__h1">New event</h1>
+          <p>Name</p>
+          <input
+            className="login__container--input"
+            placeholder="  Select a type of project"
+          ></input>
+          <p>Start</p>
+          <input
+            className="login__container--input"
+            placeholder="  Enter a deadline"
+          ></input>
+          <p>End</p>
+          <input
+            className="login__container--input"
+            placeholder="  Please, select a calendar"
+          ></input>
+          <button className="btn btn-primary">Create Project</button>
+        </div>
       </Layout>
     </>
   )
