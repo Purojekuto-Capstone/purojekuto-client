03adf25d8a89c07a610c3c28f4e0aecc54e8afdc
Antonio DroCode <antuansabe@gmail.com>
2021-07-01 17:10:42 -0500

    feat(statistics):add statistics

--- src/pages/statistics.jsx
+++ src/pages/statistics.jsx
@@ -2,6 +2,59 @@ import React from 'react';
 import Head from 'next/head';
 import Layout from '../components/layout/layout';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid } from 'recharts';
+import { RadialBarChart, RadialBar, ResponsiveContainer } from 'recharts';
+const style = {
+  top: '50%',
+  right: 0,
+  transform: 'translate(0, -50%)',
+  lineHeight: '24px',
+};
+
+const grap = [
+  {
+    name: '18-24',
+    uv: 31.47,
+    pv: 2400,
+    fill: '#8884d8',
+  },
+  {
+    name: '25-29',
+    uv: 26.69,
+    pv: 4567,
+    fill: '#83a6ed',
+  },
+  {
+    name: '30-34',
+    uv: 15.69,
+    pv: 1398,
+    fill: '#8dd1e1',
+  },
+  {
+    name: '35-39',
+    uv: 8.22,
+    pv: 9800,
+    fill: '#82ca9d',
+  },
+  {
+    name: '40-49',
+    uv: 8.63,
+    pv: 3908,
+    fill: '#a4de6c',
+  },
+  {
+    name: '50+',
+    uv: 2.63,
+    pv: 4800,
+    fill: '#d0ed57',
+  },
+  {
+    name: 'unknow',
+    uv: 6.67,
+    pv: 4800,
+    fill: '#ffc658',
+  },
+];
+
 const data = [{name: 'Proyecto 1', uv: 40, pv: 2400, amt: 2400}, 
 {
   name: 'Proyecto 2',
@@ -25,6 +78,7 @@ export default function Statistics() {
       </Head>
 
       <Layout>
+      <div className="box__container--description"> 
       <BarChart width={600} height={300} data={data}>
         <XAxis dataKey="name" stroke="#F89F29" />
         <YAxis />
@@ -33,6 +87,21 @@ export default function Statistics() {
         <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
         <Bar dataKey="uv" fill="#F89F29" barSize={30} />
   </BarChart>
+      </div>
+  <div className="box__container--calendar"> 
+      <ResponsiveContainer width="100%" height="100%">
+        <RadialBarChart cx="50%" cy="50%" innerRadius="10%" outerRadius="80%" barSize={10} data={grap}>
+          <RadialBar
+            minAngle={15}
+            label={{ position: 'insideStart', fill: '#fff' }}
+            background
+            clockWise
+            dataKey="uv"
+          />
+          <Legend iconSize={10} layout="vertical" verticalAlign="middle" wrapperStyle={style} />
+        </RadialBarChart>
+      </ResponsiveContainer>
+  </div>    
       </Layout>
     </>
   );
