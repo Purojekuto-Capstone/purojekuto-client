db429dcff6f73b56e965716089d347f6164799cb
Johnnatan Reyes Garcia <tanamix22@gmail.com>
2021-07-02 01:25:07 -0500

    change in input newProject

--- src/pages/project/[project]/event/[event].jsx
+++ src/pages/project/[project]/event/[event].jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 import Layout from '../../../../components/layout/layout'
+import Link from 'next/link';
 
 export default function EventDetail(props) {
   const router = useRouter()
@@ -8,7 +9,16 @@ export default function EventDetail(props) {
   return (
     <>
       <Layout>
-        detalles de evento {project} {event}
+        {/* detalles de evento {project} {event} */}
+        <div className="container__details">
+        <h1>Project Detail</h1>
+        <h4>Projecto 1</h4>
+        <p>Descripcion detallada del proyecto</p>
+        <p>Fecha de inicio   /   Fecha final</p>
+        <button className="btn__btn-primary"><Link href="/" >
+            <a>←Back</a>
+        </Link></button>
+    </div>
       </Layout>
     </>
   )
