b7b3c12232ee195a49b59cf41b2e5ab7b5cf661b
Anthony Gonzalez <thomasnrggo@gmail.com>
2021-07-12 15:54:50 -0500

    login responsive fix

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -5,7 +5,7 @@ import { faMoon, faSun } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import RedirectPage from '../components/redirect/redirect.jsx';
 import { useLocalStorage } from '../hooks/useLocalStorage.jsx';
-import Logo from '../../public/assets/images/Purojekuto-light.png'
+import Logo from '../../public/assets/images/Purojekuto-light.png';
 
 export default function Login() {
   const router = useRouter();
@@ -19,10 +19,10 @@ export default function Login() {
       : dispatch({ type: 'THEME__TRIGGER', payload: 'dark' });
   };
 
-  let handleLogion = () => {
-      let endpoint = 'https://purojekuto-backend.herokuapp.com/login'
-      router.push(endpoint)
-  }
+  let handleLogion = () => {
+    let endpoint = 'https://purojekuto-backend.herokuapp.com/login';
+    router.push(endpoint);
+  };
 
   if (isAuth) {
     return <RedirectPage path="/" />;
@@ -43,34 +43,35 @@ export default function Login() {
 
         <div className={`main__container `}>
           <div className="login__container">
-
-            <div className='logo__container'>
-              <img src={Logo} alt='Purojekuto'/>
+            <div className="logo__container">
+              <img src={Logo} alt="Purojekuto" />
             </div>
 
             <h2 className="login__container--p">Your personal manager app.</h2>
-             
-            <a href='https://purojekuto-backend.herokuapp.com/login'>
-                <button className="login__container--btn">
+
+            <a href="https://purojekuto-backend.herokuapp.com/login">
+              <button className="login__container--btn">
                 <div className="google-btn">
-                    <div className="google-icon-wrapper">
+                  <div className="google-icon-wrapper">
                     <img
-                        className="google-icon"
-                        src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                      className="google-icon"
+                      src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
                     />
-                    </div>
-                    <p className="btn-text">
+                  </div>
+                  <p className="btn-text">
                     <b>Sign in with google</b>
-                    </p>
+                  </p>
                 </div>
-                </button>
-                <p className="terms"> By Sign In you need to read and accept <a href="https://termify.io/terms-and-conditions-generator">Terms and Condicions</a> </p>
-                </a>
-            
-            <p className="login__container--p2">
-              {' '}
-          
-            </p>
+              </button>
+              <p className="terms">
+                By Sign In you need to read and accept{' '}
+                <a href="https://termify.io/terms-and-conditions-generator">
+                  Terms and Condicions
+                </a>{' '}
+              </p>
+            </a>
+
+            <p className="login__container--p2"> </p>
           </div>
         </div>
       </div>
